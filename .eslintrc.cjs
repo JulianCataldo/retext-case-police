5fa65f982c36d0be710aad84d26c9c5bd98000b2
Julian Cataldo <contact@juliancataldo.com>
2022-08-26 07:07:49 +0200

    chore: init 🎢

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+/** @type {import("@types/eslint").Linter.Config} */
+
+module.exports = {
+  extends: [
+    './node_modules/@julian_cataldo/astro-configs/eslint-js.cjs',
+    './node_modules/@julian_cataldo/astro-configs/eslint-ts.cjs',
+  ],
+};
